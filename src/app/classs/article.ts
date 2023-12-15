994ed64666dbd636f9b97b026faa09e7a0d30d5e
AC75009036 <loic.edouowona@acoss.fr>
2023-12-15 15:55:30 +0100

    gfgvf

---
+++ src/app/classs/article.ts
@@ -0,0 +1,12 @@
+export class Article{
+  image!: string;
+  statut!: Date;
+  title!: string;
+  bandeau!: string;
+  vue!: number;
+  like!: number;
+  repub!: number;
+  commentaire!:number;
+  
+
+}
